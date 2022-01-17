9677d5e0d24dafb376d36bf9c3885392cfde4fc8
arsha-123 <arshaashok850@gmail.com>
2022-01-17 16:07:38 +0530

    changed

--- project8/settings.py
+++ project8/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i@(gqd$%39bd^4zwl10i!ziho4!3mt97==b!i4#%s3d^f!9vl!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
